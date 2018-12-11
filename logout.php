da69e7d25e317d8a9d73c3fe2e8ee57c0eca2baf
KrSunshine <44590839+KrSunshine@users.noreply.github.com>
2018-12-11 11:25:44 +0000

    Update logout.php
    Add login button

--- logout.php
+++ logout.php
@@ -1,4 +1,14 @@
 <?php
     session_destroy();
-    echo "You have been logged out";
 ?>
+
+<!DOCTYPE html>
+<html>
+<title>Log out successful</title>
+    <form action="login.php" method="post">
+        <div class="container">
+           <h1>You have been logged out</h1>
+            <button type="submit">Log in</button>
+        </div>
+    </form>
+</html>
